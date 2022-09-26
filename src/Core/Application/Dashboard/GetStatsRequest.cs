e62e0f40778a881fb74799b77461ceb262c72500
hoaithanh-09 <1851120014@sv.ut.edu.vn>
2022-09-26 10:00:33 +0700

    Add project files.

---
+++ src/Core/Application/Dashboard/GetStatsRequest.cs
@@ -0,0 +1,52 @@
+﻿using FSH.WebApi.Application.Identity.Roles;
+using FSH.WebApi.Application.Identity.Users;
+
+namespace FSH.WebApi.Application.Dashboard;
+
+public class GetStatsRequest : IRequest<StatsDto>
+{
+}
+
+public class GetStatsRequestHandler : IRequestHandler<GetStatsRequest, StatsDto>
+{
+    private readonly IUserService _userService;
+    private readonly IRoleService _roleService;
+    // private readonly IReadRepository<Brand> _brandRepo;
+    private readonly IStringLocalizer _t;
+
+    public GetStatsRequestHandler(IUserService userService, IRoleService roleService, IStringLocalizer<GetStatsRequestHandler> localizer)
+    {
+        _userService = userService;
+        _roleService = roleService;
+       // _brandRepo = brandRepo;
+        _t = localizer;
+    }
+
+    public async Task<StatsDto> Handle(GetStatsRequest request, CancellationToken cancellationToken)
+    {
+        var stats = new StatsDto
+        {
+            // ProductCount = await _productRepo.CountAsync(cancellationToken),
+            UserCount = await _userService.GetCountAsync(cancellationToken),
+            RoleCount = await _roleService.GetCountAsync(cancellationToken)
+        };
+
+        int selectedYear = DateTime.UtcNow.Year;
+        double[] productsFigure = new double[13];
+        double[] brandsFigure = new double[13];
+        for (int i = 1; i <= 12; i++)
+        {
+            int month = i;
+            var filterStartDate = new DateTime(selectedYear, month, 01).ToUniversalTime();
+            var filterEndDate = new DateTime(selectedYear, month, DateTime.DaysInMonth(selectedYear, month), 23, 59, 59).ToUniversalTime(); // Monthly Based
+
+            // var brandSpec = new AuditableEntitiesByCreatedOnBetweenSpec<Brand>(filterStartDate, filterEndDate);
+
+            // brandsFigure[i - 1] = await _brandRepo.CountAsync(brandSpec, cancellationToken);
+        }
+
+        // stats.DataEnterBarChart.Add(new ChartSeries { Name = _t["Brands"], Data = brandsFigure });
+
+        return stats;
+    }
+}
---
+++ src/Infrastructure/Persistence/Configuration/Catalog.cs
@@ -0,0 +1,17 @@
+﻿using Finbuckle.MultiTenant.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FSH.WebApi.Infrastructure.Persistence.Configuration;
+
+//public class BrandConfig : IEntityTypeConfiguration<Brand>
+//{
+//    public void Configure(EntityTypeBuilder<Brand> builder)
+//    {
+//        builder.IsMultiTenant();
+
+//        builder
+//            .Property(b => b.Name)
+//                .HasMaxLength(256);
+//    }
+//}
